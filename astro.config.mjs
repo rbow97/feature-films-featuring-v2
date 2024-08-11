67966e2100272b4459c5840ca4cca8daac6eaf36
Rose Bowen <rosebowen1997@gmail.com>
2024-08-11 19:50:10 +0100

    adding @astrojs/vercel aaahhh

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'hybrid',
+  adapter: vercel({
+    edgeMiddleware: true,
+  }),
   integrations: [
     tailwind(),
     preact(
