9d3d0321899a3d2badee383ff92e97e3af1125ee
Rose Bowen <rosebowen1997@gmail.com>
2024-08-11 07:44:35 +0100

    remove vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/edge";
 import preact from "@astrojs/preact";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
   integrations: [tailwind(), preact({ compat: true }, { devtools: true })],
 });
