efcc60e161bb95986f087803bd048bd49a9bd3ea
Rose Bowen <rosebowen1997@gmail.com>
2024-08-11 18:18:26 +0100

    manually adding vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
+  adapter: vercel(),
   integrations: [
     tailwind(),
     preact(
@@ -18,5 +18,4 @@ export default defineConfig({
       }
     ),
   ],
-  adapter: vercel(),
 });
