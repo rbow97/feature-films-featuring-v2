a84d07805c624fd7fda24b038c6b0fb570ec4303
Rose Bowen <rosebowen1997@gmail.com>
2024-08-14 12:32:56 +0100

    remove vercel

--- .vercel/output/static/_astro/taggingSystemStore.DBv9rmBb.js
+++
@@ -1,1 +0,0 @@
-import{a as s}from"./newSystem.Dax5KQaO.js";const a=s([]);export{a as c};
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
-  adapter: vercel(),
   integrations: [
     tailwind(),
     preact(
